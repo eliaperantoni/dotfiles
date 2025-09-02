ffa7d24a7af61fc7c0dff26b21216fea64f61f02
Elia Perantoni <perantonielia0@gmail.com>
2025-09-02 23:18:06 +0200

    feat: VSCode nvim

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -3,16 +3,4 @@
 -- Add any additional options here
 
 vim.g.lazyvim_python_lsp = "basedpyright"
-vim.g.clipboard = {
-  name = "WslClipboard",
-  copy = {
-    ["+"] = "clip.exe",
-    ["*"] = "clip.exe",
-  },
-  paste = {
-    ["+"] = 'powershell.exe -NoLogo -NoProfile -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-    ["*"] = 'powershell.exe -NoLogo -NoProfile -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-  },
-  cache_enabled = 0,
-}
 vim.g.root_spec = { "cwd", ".git" }
