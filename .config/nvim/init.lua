1e893497bfa946b569d2bce41ff209781307566a
Elia Perantoni <elia@twig.energy>
2026-07-14 10:02:34 +0200

    Filepath relative

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -93,6 +93,7 @@ vim.pack.add({ gh("nvim-lualine/lualine.nvim") })
 require("lualine").setup({
 	options = { section_separators = "", component_separators = "" },
 	sections = {
+		lualine_c = { { "filename", path = 1 } },
 		lualine_x = { "%S" },
 	},
 })
